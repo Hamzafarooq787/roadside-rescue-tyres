f5ef1b021f619960e72a736d1fd4eae366c23223
Hamzafarooq787 <hamza.farooq5060@gmail.com>
2026-03-09 23:33:34 +0500

    Convert next.config.ts to next.config.js for Vercel

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["mobiletyresnationwide.co.uk", "images.unsplash.com"],
-  },
-};
-
-export default nextConfig;
